03fd956ccf04234dff684c3dfb950db2ae17cc8f
chadmuro <chadmuro@gmail.com>
2023-05-26 23:20:29 +0900

    fix: Update getlastdays function logic to fill week

--- utils/getLast365Days.ts
+++ utils/getLast365Days.ts
@@ -3,7 +3,7 @@ import dayjs from "dayjs";
 export function getLast365Days(today: dayjs.Dayjs) {
   const dates: string[] = [];
   const dayOfTheWeek = today.day();
-  const previousDays = 365 - dayOfTheWeek + 1;
+  const previousDays = 365 - 7 + dayOfTheWeek;
 
   for (let i = 0; i < previousDays; i++) {
     dates.unshift(today.subtract(i, "days").format("YYYY-MM-DD"));
